38222705556250d4b7fde564cabc5b52f2c0feec
ybh <2616442@naver.com>
2019-04-30 15:21:08 +0900

    zz

--- 01/ViewController.swift
+++ 01/ViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var lable: UILabel!
+    
+    @IBOutlet weak var btA: UIButton!
+    
+    @IBOutlet weak var btB: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBAction func btAc(_ sender: Any) {
+        lable.text = "버튼 A가 눌러짐"
+    }
+    
+    @IBAction func btBc(_ sender: Any) {
+        lable.text = "버튼 B가 눌러짐"
+    }
 }
 
---
+++ table3/table3/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  table3
+//
+//  Created by D7703_22 on 2019. 4. 30..
+//  Copyright © 2019년 D7703_22. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource{
+    
+    
+    @IBOutlet weak var myTableView: UITableView!
+    var animals = ["cat","dog","pig","bird","whale","cow"]
+    var dataAnimal = ["고양이","강아지","돼지","새","돌고래","말"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        myTableView.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
+        return animals.count
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        //cell 재활용을 위해....
+        let myCell = myTableView.dequeueReusableCell(withIdentifier:"RE", for:indexPath)
+        let myRow = indexPath.row
+        myCell.textLabel?.text = animals[myRow]
+        myCell.detailTextLabel?.text = dataAnimal[myRow]
+        myCell.imageView?.image = UIImage(named: animals[myRow])
+        return myCell
+    }
+
+
+}
+
